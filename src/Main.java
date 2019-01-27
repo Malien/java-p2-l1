c1bb5151efb3cb05153cd468b3f1bcd037da5b2c
Yaroslav <yaroslav.petryk@ukma.edu.ua>
2019-01-27 14:20:00 +0200

    Repo cleanup. Still some work to do

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import static Utility.DataInput.getString;
+
 public class Main {
-
     public static void main(String[] args){
+        KMA launcher = new KMA();
+        while(true) {
+            int keepOn = askForChange(launcher);
+            if (keepOn == 0) break;
+        }
 
+        launcher.addTeachers();
+        launcher.addStudents();
+    }
+
+
+    private static int askForChange(KMA l) {
+        String change = "";
+        try {
+            change = getString("Would you like to make some changes to faculties? ");
+        }catch (Exception e){}
+        if (change.equals("Yes")) l.changeFaculty();
+        else return 0;
+
+        //ask if the user wants to stay in changing menu
+        String carryOn = getString("Do you want to make some more changes? ");
+        if (carryOn.equals("Yes")) return 1;
+        return 0;
     }
 }
---
+++ src/Utility/DataInput.java
@@ -0,0 +1,61 @@
+package Utility;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public final class DataInput {
+
+	private static void writeText(String wr){
+		if (wr == null)
+			System.out.print("Введіть дані: ");
+		else 
+			System.out.print(wr);
+	}
+	
+	public static Long getLong() throws IOException{
+		String s = getString();
+		Long value = Long.valueOf(s);
+		return value;
+	}
+	
+	public static char getChar() throws IOException{
+		String s = getString();
+		return s.charAt(0);
+	}
+	
+	public static Integer getInt(String wr){
+		writeText(wr);
+		String s = "";
+		try {
+			s = getString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Integer value = Integer.valueOf(s);
+		return value;
+		
+	}
+	
+	public static String getString() throws IOException{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+
+	public static String getString(String str){
+		try{
+			System.out.print(str);
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr);
+			String s = br.readLine();
+			return s;
+		} catch (IOException e){
+			System.out.println(e.getStackTrace());
+			return "";
+		}
+	}
+	
+}
